d9e9a722297fe43f183fcdd2ad9289521bc54397
Arshadkolkar <kolkar.arshad@gmail.com>
2017-05-01 13:32:59 +0530

    landing Page added

---
+++ landing.php
@@ -0,0 +1,229 @@
+<?php 
+include'database.php';
+
+
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+
+<html> 
+<head>
+   <link rel="stylesheet" href="css/other.css">
+   <link rel="stylesheet" href="css/input.css">
+</head>
+<title></title>
+<style type="text/css">
+
+</style>
+<body bgcolor="#2c3e50">
+ 
+
+			<div class="heading" id="s">
+					<img src="1404.png" >
+					<font color="white"> <h1> <span class="gothic black-text">CRACKING THE</span> MACHINE LEARNING CODE</h1>
+					From powering self driving cars and recommendation engines, to enhancing cybersecurity,machine</br>
+					learning is one of the biggest breakthroughs of the digital age.
+					Sign up</br> for a week long lecture series and deep dive into the fascinating </br>
+					world of the hottest technology today</font>
+                     
+                    </div>
+					
+
+
+<div id="main">
+	<h2 align='center'>For Enquiries</h2><br>
+   <h1 align='center'>1800-123-1260</h1><br>
+  
+   <h4 align='center'>ONLINE, LIVE & INTERACTIVE</h4><br>
+   <h5 align='center'>30th APRIL 2017 - 6th MAY 2017</h5><br>
+   
+  <!-- ---------------------------------Code Here--------------------------------- -->
+ <center>
+			<form action="process.php" method="POST">
+         
+					<p>	<input type="text" name="fname" placeholder="First Name" /></p><br>
+					<p><input  type="text" name="lname" placeholder="Last Name" /></p><br>
+					<p><input  type="email" name="email" placeholder="Email" /></p><br>
+					<p><input  type="text" name="phno" placeholder="Phone" /></p><br>	
+					<p class="keeplogin"> 
+					<input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping" /> 
+					<label for="loginkeeping">Keep me logged in</label><br>
+				</p><br>
+	            <input type="submit" name="submit" value="REGISTER" />	            			
+        </form>
+	</center>
+	
+  <!-- ---------------------------------Code Here--------------------------------- -->
+</div>
+
+ 
+               <div align="center">
+                   <img src="140.png" align="center" >
+                   <h1 align="center">Why Attend?</h1>
+               
+<table border="0",align="center",cellpadding="80" cellspacing="80" >
+<tr>
+						<td><img src="1405.png">
+                        <p>Know about the opportunities &amp;</br>
+						growth prospects in a</br> 
+						Machine Learning career</p></td>
+<td><img src="1406.png">
+                        <p>Understand the emerging trends</br>
+							&amp; learning paths in 
+							Machine</br> 
+							Learning ecosystem</p></td>
+<td><img src="1407.png" >
+                        <p>Get insights about applications &amp;</br>
+							case studies from best minds in</br>
+								the industry</p></td>
+<td><img src="1408.png" >
+                        <p>Get career advise &amp;</br>
+						tips tocrackMachine </br>Learning roles</p></td>
+</tr>
+
+</table>
+</div>
+</br>
+
+ <div align="center">
+					
+                   <img src="401.png" align="center" ><h1>SPEAKERS</h1>
+				   <hr width="55%"></hr>
+                   
+          
+<table border="0" align="center" cellspacing="20" >
+<tr>
+<td align="center"> <h4 class="raleway text-center white-text bold-text">Mukesh Jain</h4>
+                        <p class="white-text text-center raleway light-text">Ex-VP &amp; Head <br>Big Data Analytics<br>Reliance Jio</p></td>
+<td align="center"><h4 class="raleway text-center white-text bold-text">Ratnakar Pandey</h4>
+                        <p class="white-text text-center raleway light-text">India Head,Analytics<br>Kabbage Inc <br>MBA, ISB</p></td>
+<td align="center"><h4 class="raleway text-center white-text bold-text">Rajeev Kumar</h4>
+                        <p class="white-text text-center raleway light-text">Head Google Projects<br>IBM</p></td>
+</tr>
+<tr>
+<td align="center"><h4 class="raleway text-center white-text bold-text">Rajath K M</h4>
+                        <p class="white-text text-center raleway light-text">Machine Learning Specialist<br>IIT Madras<br>Hyperverge Inc</p></td>
+<td align="center"><h4 class="raleway text-center white-text bold-text">Akshay Gupta</h4>
+                        <p class="white-text text-center raleway light-text">Senior Research Engineer<br>Applied Machine Learning<br>E-bay</p></td>
+<td align="center"> <h4 class="raleway text-center white-text bold-text">Kunal Jain</h4>
+                        <p class="white-text text-center raleway light-text">Founder<br>Analytics Vidhya<br>IIT, Bombay </p></td>
+</tr>
+<tr>
+<td align="center"><h4 class="raleway text-center white-text bold-text">Manvender Singh
+</h4>
+                        <p class="white-text text-center raleway light-text">Founder &amp; CEO<br>UpX Academy<br>MBA, ISB</p></td>
+<td align="center">  <h4 class="raleway text-center white-text bold-text">Suchit Majumdar</h4>
+                        <p class="white-text text-center raleway light-text">IoT &amp; Smart City consultant <br>with AP Government<br>MBA, ISB</p></td>
+<td align="center"><h4 class="raleway text-center white-text bold-text">Arun K</h4>
+                        <p class="white-text text-center raleway light-text">Healthcare Analytics Expert<br>University of Cologne</p></td>
+</tr>
+
+</table>
+
+</div>
+</br>
+<section id="student-speak">
+            <div class="container" align="center">
+                <div class="section-heading">
+				</br>
+                    <h2 class="white-text text-center roboto light-text">UpX Student Speak</h2>
+					</br>
+					<hr width="40%"></hr>
+					
+                </div>
+                <div class="speaks">
+                  
+                    <div align="center" class="speak">
+                        <p">UpX Academy has understood the Big Data Analytics space fairly well and are doinga brilliant</br>job in educating 
+young leaders and professionals who look forward to join this sector.</p>
+                        <p>- Jai Kumar - Director, BI, NTT DATA</p>
+                    </div>
+					<hr width="40%"></hr>
+                    <div align="center" class="speak">
+                        <p>UpX Academy has excellent faculty on-board that brings in industry perspective in their programs.</br> The five week 
+training they conducted has helped my team adopt the latest Big Data Analytics practices. </p>
+                        <p class="author white-text text-right roboto light-text">- Puneet Sakhuja - BI, Analytics - Practice leader, Tech Mahindra</p>
+                    </div>
+					<hr width="40%"></hr>
+                    <div  align="center" class="speak">
+                        <p>Initially, we were a bit apprehensive about online trainings.</br>But the interactive approach,support and the way the 
+trainings were organized impressed my team.</br> The pace of the trainingwas perfect and suitable for even beginners 
+in this area.</p>
+                        <p>- Siddharth Bhattacharya - AVP Analytics, Barclays</p>
+                    </div>
+                </div>
+            </div>
+			</br>
+			</br>
+        </section>
+
+<section id="about" align="center">
+            <div align="center">
+                <div align="center" >
+				</br>
+                    <h1>About UpX Academy</h1>
+					</br>
+					</br>
+					<hr width="40%"></hr>
+                </div>
+                <p>In todays digital age, data has the potential to transform businesses through insights and </br>
+				analytics. However, this power still awaits to be fully harnessed. As a world-class educational </br>
+				venture, we recognize this challenge and are therefore committed to empower professionals </br>
+				through transformational courses in Big Data and Data Science. 
+</p>
+<hr width="40%"></hr>
+            </div>
+			</br>
+
+        </section>
+
+
+<section id="our-courses" align="center">
+            <div class="container">
+                <div class="section-heading">
+                    <h2>Our Courses</h2>
+                </div>
+				 </section>
+				 <div align="center" id="our-courses">
+				
+				<table border="0" align="center" cellspacing="08" >
+				<tr>
+				<td>
+                
+                        <h4 >Big Data
+                        Hadoop</h4>
+						<h4>Apache Spark
+                        Mongo DB</h4>
+                        <a href="https://upxacademy.com/" ><P align="center"  >Know More</P></a>
+                     </br>
+                   				
+					</td>
+					
+					
+					<td>
+                    
+                        <h4>Data Science
+                        Data Analytics</h4>
+                        <h4>Machine Learning
+                        Tableau</h4>
+                        <a href="https://upxacademy.com/"><p align="center">Know More</p></a>
+						</br>
+					
+                        
+                   
+                
+            </td>
+			</tr>
+			</table>
+			</br>
+			</br>
+       </div>
+
+  </br>
+  </br>
+  
+  
+</body>
+</html>
